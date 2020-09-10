bc0629fd7c10cf6070e5fd0b3fedb8076b4a74b6
Eric Broadbent <Broadbent52@aol.com>
2020-09-10 16:13:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+class Main {
+  public static void main(String[] args) {
+    //has to be in the {}
+    System.out.println("Hello world!");
+    System.out.println("Aaron Hales");
+    System.out.println("Jaiden A Lewis");
+
+    
+    System.out.println("Jaxyn");
+
+        System.out.println("Nate WildeEric");
+        System.out.println("sean fullmer ");
+        System.out.println("Trevor");
+
+    System.out.println("Jason")
+    System.out.println("Holden Anderson");
+    System.out.println("Ryan");
+    System.out.println("Lydia");
+    System.out.println("Olivia");
+    //must not be below the 
+  }
+}
+
+   System.out.println("labron james")
+
+
+
+
+System.out.print("sean fullmer")
