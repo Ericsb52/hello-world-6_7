f395545e377f0589eefec390b588d0b0acdc6e67
Eric Broadbent <Broadbent52@aol.com>
2020-09-10 16:14:07 +0000

    first push

--- Main.java
+++ Main.java
@@ -4,26 +4,20 @@ class Main {
     System.out.println("Hello world!");
     System.out.println("Aaron Hales");
     System.out.println("Jaiden A Lewis");
-
+    System.out.println("labron james");
     
     System.out.println("Jaxyn");
 
-        System.out.println("Nate WildeEric");
-        System.out.println("sean fullmer ");
-        System.out.println("Trevor");
-
-    System.out.println("Jason")
+    System.out.println("Nate Wilde");
+    System.out.println("sean fullmer ");
+    System.out.println("Trevor");
+  
+    System.out.println("Jason");
     System.out.println("Holden Anderson");
     System.out.println("Ryan");
     System.out.println("Lydia");
     System.out.println("Olivia");
-    //must not be below the 
   }
 }
 
-   System.out.println("labron james")
-
-
-
-
-System.out.print("sean fullmer")
+  System.out.println(my)
